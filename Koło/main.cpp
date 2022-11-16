b1cd2fab75794f9ab8124c5b0e14cd44c256cb04
Mikołaj Mejer <73598279+randomLando@users.noreply.github.com>
2022-11-16 15:37:59 +0100

    Add files via upload

---
+++ Koło/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define PI 3.14159
+using namespace std;
+
+    int main()
+    {
+    	float r, p, ob;
+    	cin>>r;
+
+		ob = 2*PI*r;
+		p = PI*(r*r);
+        cout<< p << endl;
+        cout<< ob << endl;
+
+        cout << endl;
+        return 0;
+    }
